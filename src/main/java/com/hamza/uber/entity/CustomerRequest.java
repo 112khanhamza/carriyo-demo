714cbba56de311066c4c62b65ab5b6df687b65c4
112khanhamza <112khanhamza@gmail.com>
2023-07-17 15:35:25 +0530

    Deleted Code

--- src/main/java/com/hamza/uber/entity/CustomerRequest.java
+++ src/main/java/com/hamza/uber/entity/CustomerRequest.java
@@ -1,16 +1,4 @@
 package com.hamza.uber.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
 public class CustomerRequest {
-    private String carModel;
-    private double distance;
-    private String location;
 }
--- src/main/java/com/hamza/uber/entity/Driver.java
+++ src/main/java/com/hamza/uber/entity/Driver.java
@@ -1,21 +1,4 @@
 package com.hamza.uber.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-import java.util.List;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 public class Driver {
-    // Prams -> driver, carModel, ratings, distanceFromCustomer
-    private String driver;
-    private String carModel;
-    private double rating;
-    private int distanceFromCustomer; // meters
-    private List<String> preferredLocations;
 }
